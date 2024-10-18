cc43e71febb6b4ce93ea8f4e8ad61afc1d538492
NUBsGP <nubsgpbr@hotmail.com>
2024-10-18 16:49:01 -0300

    estudos

---
+++ aula4/comentarios.js
@@ -0,0 +1,14 @@
+// comentario de linha
+console.log("abiaba"); 
+//console.log("aaa");
+
+/*comentario de bloco
+  comentario de bloco*/
+/*
+console.log("sla");
+console.log("sla");
+console.log("sla");
+console.log("sla");
+console.log("sla");
+console.log("sla");
+*/
