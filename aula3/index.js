0829b1e00a36700311a380a62c02167186790bce
NUBsGP <nubsgpbr@hotmail.com>
2024-10-18 16:00:16 -0300

    estudos

---
+++ aula3/index.js
@@ -0,0 +1,8 @@
+console.log('posso usar "" dentro do texto');
+console.log("posso usar '' dentro do texto");
+console.log(`posso usar '' e "" dentro do texto`); //usado para template string
+
+console.log(1234);
+console.log(1234.5);
+console.log(1234.5, 7987, "abiaba");
+console.log(34 + 35 + " a");
