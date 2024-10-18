baafe61af012a6a1b2bd86c587e44df4b6320428
NUBsGP <nubsgpbr@hotmail.com>
2024-10-18 17:27:34 -0300

    estudos

---
+++ aula7/index.js
@@ -0,0 +1,17 @@
+const nome = "jão"; //constantes tem valores "constantes", ou seja, valores fixos. E sempre tem que declarar com valor
+
+const numero1 = 5;
+const numero2 = 10;
+const numero3 = numero1 + numero2;
+
+console.log(numero3);
+
+let numero4 = numero3 * 2;
+
+console.log(numero4);
+
+numero4 = numero4 * 2;
+
+console.log(numero4);
+
+console.log(typeof(numero4) + " " + typeof(numero1) + " " + typeof(nome));
