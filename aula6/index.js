9d23b378003ed6b4a6b2c5fb16d38c82c9a60a2b
NUBsGP <nubsgpbr@hotmail.com>
2024-10-18 17:17:56 -0300

    estudos

---
+++ aula6/index.js
@@ -0,0 +1,10 @@
+let nome;
+nome = "jão";
+
+var nome2 = "Jãozin";
+
+console.log(
+    "ai o", nome, ", não sei oq não sei oq lá\n" +
+    "foi lá e não sei mais oq não sei oq lá\n" +
+    "ai o", nome, "morreu, press f"
+);
