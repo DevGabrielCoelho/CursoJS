09b573ace1fc85af66be5696d75cf88098662f91
NUBsGP <nubsgpbr@hotmail.com>
2024-10-19 19:47:51 -0300

    estudos

---
+++ aula13/index.js
@@ -0,0 +1,17 @@
+let s = "string";
+
+console.log(s.charAt(5));
+console.log(s.concat("s"));
+console.log(s.indexOf("ing"));
+console.log(s.lastIndexOf("s"));
+console.log(s.match(/[a-z]/g));
+console.log(s.search(/[a-z]/g));
+console.log(s.replace("str", "trs"));
+console.log(s.replace(/str/g, "trs"));
+console.log(s.length);
+console.log(s.slice(1, 4));
+console.log(s.slice(-2));
+console.log(s.substring(s.length -2));
+console.log(s.split('i'));
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
