b17d17b6056dcaea50581a25d3c64b1d8fd89c54
NUBsGP <nubsgpbr@hotmail.com>
2024-10-19 21:38:15 -0300

    estudos

---
+++ aula16/index.js
@@ -0,0 +1,39 @@
+let vet = ["a", 2, true, null];
+console.log(vet);
+console.log(vet[0]);
+
+vet[0] = "b";
+
+console.log(vet);
+console.log(vet[0]);
+
+vet[vet.length] = "novo elemento";
+
+console.log(vet);
+
+vet.push("outro metodo de add");
+
+console.log(vet);
+
+vet.unshift("Adicionar no começo");
+
+console.log(vet);
+
+vet.pop(); // removendo o ultimo elemento
+
+console.log(vet);
+
+vet.shift() // removendo o primeiro elemento
+
+console.log(vet);
+
+delete vet[1]; // removendo mas deixando vago
+
+console.log(vet);
+
+console.log(vet.slice(2, 5));
+
+console.log(typeof vet);
+
+console.log(vet instanceof Array);
+
