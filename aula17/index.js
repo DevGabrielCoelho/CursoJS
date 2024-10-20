af78d50893309101e23f5f2c30dfd31be904f377
NUBsGP <nubsgpbr@hotmail.com>
2024-10-20 04:29:16 -0300

    estudos

---
+++ aula17/index.js
@@ -0,0 +1,15 @@
+function aoba(n){
+  console.log("aoba", n);
+  return "aoba " + n;
+}
+
+let x = aoba("meu bom");
+console.log(x);
+
+const funcaolegal = function(){
+  console.log("abiaba");
+};
+
+const funcaolegal2 = () => {
+  console.log("eitaaa");
+};
