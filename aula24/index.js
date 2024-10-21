1704ea77b4836f16a4d37cd639a7b212fb565fbe
NUBsGP <nubsgpbr@hotmail.com>
2024-10-21 03:14:05 -0300

    estudos

---
+++ aula24/index.js
@@ -0,0 +1,13 @@
+let hora = 2;
+
+if (hora >= 6 && hora < 12 ){
+  console.log("Bom Dia!");
+}
+
+else if (hora >= 12 && hora < 18){
+  console.log("Boa Tarde!");
+}
+
+else{
+  console.log("Boa Noite!");
+}
