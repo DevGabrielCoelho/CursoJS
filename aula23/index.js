783199da62ec7e6461000acbeb4daf9748ee7270
NUBsGP <nubsgpbr@hotmail.com>
2024-10-21 03:08:38 -0300

    estudos

---
+++ aula23/index.js
@@ -0,0 +1,20 @@
+/*
+ *  valores que apontam false
+ *  false
+ *  0
+ *  '' "" ``
+ *  null
+ *  undefined
+ *  NaN
+ *  
+ *  não precisa necessariamente de uma estrutura logica para fazer operações
+ *  logicas:
+ *  x = 0;
+ *  y = x || 1; // y == 1
+ * 
+ *  x = 1;
+ *  y = x || 3; // y == 1
+ *  
+ *  x = false;
+ *  y = x && true; // y == false;
+ */
