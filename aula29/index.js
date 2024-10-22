2abd011e9607c654433fc415ea4dafd55248debd
NUBsGP <nubsgpbr@hotmail.com>
2024-10-22 07:01:14 -0300

    estudos

---
+++ aula29/index.js
@@ -0,0 +1,14 @@
+switch(11/*valor*/){
+  case 1:
+    console.log("vale 1");
+    break;
+  case 2:
+    console.log("vale 2");
+    break;
+  case 10:
+    console.log("vale 10");
+    break;
+  default:
+    console.log("vale um valor não previsto");
+    break;
+}
