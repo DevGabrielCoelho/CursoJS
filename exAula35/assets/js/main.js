50f0361ca94d237a2992234c7a1238c52b0f825c
NUBsGP <nubsgpbr@hotmail.com>
2024-10-31 02:43:31 -0300

    estudos

--- exAula35/assets/js/main.js
+++ exAula35/assets/js/main.js
@@ -7,7 +7,24 @@ const elementos = [
   {tag: "section", texto: "Frase 4"}
 ]
 
+// *--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---*
+
 for (let i = 0; i < elementos.length; i++){
   conteiner.innerHTML += 
   `<${elementos[i].tag}> ${elementos[i].texto} </${elementos[i].tag}>`;
 }
+
+// *--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---*
+
+const div = document.createElement("div");
+
+for (let i = 0; i < elementos.length; i++){
+  let { tag, texto} = elementos[i];
+  let tagCriada = document.createElement(tag);
+  let textoCriado = document.createTextNode(texto);
+
+  tagCriada.appendChild(textoCriado);
+  div.appendChild(tagCriada);
+}
+
+conteiner.appendChild(div);
